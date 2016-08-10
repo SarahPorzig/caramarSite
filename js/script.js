5ee546cf0d6949ff1a5bc4bc494e24f84de3f27d
Sarah Porzig <sarahporzig@icloud.com>
2016-08-10 16:09:00 +1000

    adds scroll function to nav bar buttons

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$("#propertyButton").click(function() {
+$('html, body').animate({
+    scrollTop: $("#propertyDetailsHeading").offset().top
+}, 1000);
+});
+$("#floraAndFaunaButton").click(function() {
+$('html, body').animate({
+    scrollTop: $("#floraAndFaunaHeading").offset().top
+}, 1000);
+});
+$("#horsesButton").click(function() {
+$('html, body').animate({
+    scrollTop: $("#horsesHeading").offset().top
+}, 1000);
+});
+$("#historyButton").click(function() {
+$('html, body').animate({
+    scrollTop: $("#historyHeading").offset().top
+}, 1000);
+});
