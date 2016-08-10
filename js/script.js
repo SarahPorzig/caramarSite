14e519eb1f027b8bfe0aea42803e6baca32579dc
Sarah Porzig <sarahporzig@icloud.com>
2016-08-10 16:52:05 +1000

    changes nav scroll to show section in middle of page not top. Header changes to white. Opacity on banners

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 $("#propertyButton").click(function() {
 $('html, body').animate({
-    scrollTop: $("#propertyDetailsHeading").offset().top
+    scrollTop: $("#propertyDetailsHeading").offset().top - ( $(window).height() - $("#propertyDetailsHeading").outerHeight(true)) / 2
 }, 1000);
 });
 $("#floraAndFaunaButton").click(function() {
 $('html, body').animate({
-    scrollTop: $("#floraAndFaunaHeading").offset().top
+    scrollTop: $("#floraAndFaunaHeading").offset().top - ( $(window).height() - $("#floraAndFaunaHeading").outerHeight(true)) / 2
 }, 1000);
 });
 $("#horsesButton").click(function() {
 $('html, body').animate({
-    scrollTop: $("#horsesHeading").offset().top
+    scrollTop: $("#horsesHeading").offset().top - ( $(window).height() - $("#horsesHeading").outerHeight(true)) / 2
 }, 1000);
 });
 $("#historyButton").click(function() {
 $('html, body').animate({
-    scrollTop: $("#historyHeading").offset().top
+    scrollTop: $("#historyHeading").offset().top - ( $(window).height() - $("#historyHeading").outerHeight(true)) / 2
 }, 1000);
 });
