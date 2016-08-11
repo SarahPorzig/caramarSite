fca80f77d37fe7ce198dc816db2f7d45b7ec62fb
Sarah Porzig <sarahporzig@icloud.com>
2016-08-11 15:12:14 +1000

    adds footer

--- js/script.js
+++ js/script.js
@@ -1,20 +1,26 @@
-$("#propertyButton").click(function() {
+$(".propertyButton").click(function() {
 $('html, body').animate({
     scrollTop: $("#propertyDetailsHeading").offset().top - ( $(window).height() - $("#propertyDetailsHeading").outerHeight(true)) / 2
 }, 1000);
 });
-$("#floraAndFaunaButton").click(function() {
+$(".floraAndFaunaButton").click(function() {
 $('html, body').animate({
     scrollTop: $("#floraAndFaunaHeading").offset().top - ( $(window).height() - $("#floraAndFaunaHeading").outerHeight(true)) / 2
 }, 1000);
 });
-$("#horsesButton").click(function() {
+$(".horsesButton").click(function() {
 $('html, body').animate({
     scrollTop: $("#horsesHeading").offset().top - ( $(window).height() - $("#horsesHeading").outerHeight(true)) / 2
 }, 1000);
 });
-$("#historyButton").click(function() {
+$(".historyButton").click(function() {
 $('html, body').animate({
     scrollTop: $("#historyHeading").offset().top - ( $(window).height() - $("#historyHeading").outerHeight(true)) / 2
 }, 1000);
+});
+
+$(".backToTop").click(function() {
+$('html, body').animate({
+    scrollTop: $("#brand").offset().top
+  }, 1000);
 });
