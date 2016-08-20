f4fe45c09a21924615637d03d3e1def5cd8d5ea0
Sarah Porzig <sarahporzig@icloud.com>
2016-08-20 14:46:15 +1000

    tidies up code

--- js/script.js
+++ js/script.js
@@ -1,38 +1,42 @@
-$(".propertyButton").click(function() {
-$('html, body').animate({
-    scrollTop: $("#propertyDetailsHeading").offset().top - ( $(window).height() - $("#propertyDetailsHeading").outerHeight(true)) / 2
-}, 1000);
-});
-$(".floraAndFaunaButton").click(function() {
-$('html, body').animate({
-    scrollTop: $("#floraAndFaunaHeading").offset().top - ( $(window).height() - $("#floraAndFaunaHeading").outerHeight(true)) / 2
-}, 1000);
-});
-$(".horsesButton").click(function() {
-$('html, body').animate({
-    scrollTop: $("#horsesHeading").offset().top - ( $(window).height() - $("#horsesHeading").outerHeight(true)) / 2
-}, 1000);
-});
-$(".historyButton").click(function() {
-$('html, body').animate({
-    scrollTop: $("#historyHeading").offset().top - ( $(window).height() - $("#historyHeading").outerHeight(true)) / 2
-}, 1000);
-});
+$(document).ready(function() {
 
-$(".backToTop").click(function() {
-$('html, body').animate({
-    scrollTop: $("#brand").offset().top
+  $(".propertyButton").click(function() {
+  $('html, body').animate({
+      scrollTop: $("#propertyDetailsHeading").offset().top - ( $(window).height() - $("#propertyDetailsHeading").outerHeight(true)) / 2
   }, 1000);
-});
+  });
+  $(".floraAndFaunaButton").click(function() {
+  $('html, body').animate({
+      scrollTop: $("#floraAndFaunaHeading").offset().top - ( $(window).height() - $("#floraAndFaunaHeading").outerHeight(true)) / 2
+  }, 1000);
+  });
+  $(".horsesButton").click(function() {
+  $('html, body').animate({
+      scrollTop: $("#horsesHeading").offset().top - ( $(window).height() - $("#horsesHeading").outerHeight(true)) / 2
+  }, 1000);
+  });
+  $(".historyButton").click(function() {
+  $('html, body').animate({
+      scrollTop: $("#historyHeading").offset().top - ( $(window).height() - $("#historyHeading").outerHeight(true)) / 2
+  }, 1000);
+  });
 
-/* changes breakpoint of navbar to medium rather than small - works with media query in css doc*/
-$('#monitor').html($(window).width());
+  $(".backToTop").click(function() {
+  $('html, body').animate({
+      scrollTop: $("#brand").offset().top
+    }, 1000);
+  });
 
-$(window).resize(function() {
-    var viewportWidth = $(window).width();
-    $('#monitor').html(viewportWidth);
-});
+  /* changes breakpoint of navbar to medium rather than small - works with media query in css doc*/
+  $('#monitor').html($(window).width());
 
-$('.nav a').on('click', function(){
-    $('.navbar-toggle').click()
+  $(window).resize(function() {
+      var viewportWidth = $(window).width();
+      $('#monitor').html(viewportWidth);
+  });
+
+  $('.nav a').on('click', function(){
+      $('.navbar-toggle').click()
+  });
+
 });
